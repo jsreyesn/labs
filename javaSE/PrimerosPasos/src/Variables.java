d66329ca06c0a5aa8521465956de150ef4eab622
jsreyesn <jsreyesn@areadit.com>
2018-08-21 21:05:56 -0500

    Se añade clase variables

---
+++ javaSE/PrimerosPasos/src/Variables.java
@@ -0,0 +1,19 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		byte edad = 35; // Declaracion e iniciacion de variable edad
+		
+		/* Instruccion imprime en la consola
+		 * el valor de la variable edad
+		 */		
+		System.out.println(edad);
+		
+		edad = 75;
+		
+		System.out.println(edad);
+		
+	}
+
+}
