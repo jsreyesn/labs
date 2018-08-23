25298cd521aafaac0703d1257d847e742a2ff782
jsreyesn <jsreyesn@areadit.com>
2018-08-22 19:26:25 -0500

    Talleres pildotas informaticas

---
+++ javaSE/PrimerosPasos/src/Calculos_conMath.java
@@ -0,0 +1,20 @@
+
+public class Calculos_conMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//double raiz=Math.sqrt(9);
+		
+		double num1=5.85;		
+		int resultado=(int)Math.round(num1);
+		
+		double base=5;
+		double exponente=3;
+		int resultado1=(int)Math.pow(base, exponente);
+		
+		System.out.println("El resultado de "+base+" elevado a "+exponente+" es "+resultado1);
+
+	}
+
+}
