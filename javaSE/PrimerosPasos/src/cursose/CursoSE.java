52df9934555d2fcda95c2aea10758d1e3351e434
jsreyesn <jsreyesn@areadit.com>
2018-08-23 18:53:46 -0500

    Taller Mitocode Condicionales

--- javaSE/PrimerosPasos/src/cursose/CursoSE.java
+++ javaSE/PrimerosPasos/src/cursose/CursoSE.java
@@ -5,7 +5,7 @@ public class CursoSE {
 	public static void main(String... mitocode) {
 		// TODO Auto-generated method stub
 		
-		int x = 20;
+		int x = 70;
 		
 		if(x > 50 == true) {
 			System.out.println("Esta es una sentencia if");
