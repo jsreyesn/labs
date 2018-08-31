c76ad82947ecf438a7c77a23e8520380295f1ad6
jsreyesn <jsreyesn@areadit.com>
2018-08-31 07:52:45 -0500

    Pildoras: Practica final Arrays

--- javaSE/PrimerosPasos/src/Arrays_bidimensionales.java
+++ javaSE/PrimerosPasos/src/Arrays_bidimensionales.java
@@ -4,24 +4,20 @@ public class Arrays_bidimensionales {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[][] matrix = new int[2][3];
+		int[][] matrix = {
+				{10,15,18,19,21},
+				{5,25,37,41,15}
+		};
 		
-		matrix[0][0] = 15;
-		matrix[0][1] = 10;
-		matrix[0][2] = 19;
-		
-		matrix[1][0] = 21;
-		matrix[1][1] = 18;		
-		matrix[1][2] = 9;
-		
-		for(int i=0;i<2;i++) {
-			
-			for(int j=0;j<3;j++) {
-				System.out.print(matrix[i][j]+" ");
-			}
+		for(int[]fila:matrix) {
 			
 			System.out.println();
+			
+			for(int z:fila) {
+				System.out.print(z+" ");
+			}
 		}
+		
 
 	}
 
