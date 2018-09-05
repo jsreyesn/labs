7e8cf69d94157df36ec4387b0b10c476c42f05e4
jsreyesn <jsreyesn@areadit.com>
2018-09-04 20:25:00 -0500

    Practica Objetos

--- javaSE/PrimerosPasos/src/poo/Uso_Coche.java
+++ javaSE/PrimerosPasos/src/poo/Uso_Coche.java
@@ -1,13 +1,32 @@
 package poo;
 
+import javax.swing.*;
+
 public class Uso_Coche {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Coche Renault = new Coche(); // INSTANCIAR UNA CLASE. EJEMPLAR DE CLASE
-			
-		System.out.println("Este coche tiene "+Renault.ruedas+" ruedas.");
+		Coche micoche = new Coche();
+		
+		micoche.establece_color(JOptionPane.showInputDialog("Introduce Color"));
+		
+		System.out.println(micoche.dime_datos_generales());		
+		
+		System.out.println(micoche.dime_color());
+		
+		micoche.configura_asientos(JOptionPane.showInputDialog("¿Tiene asientos de cuero?"));
+		
+		System.out.println(micoche.dime_asientos());
+		
+		micoche.configura_climatizador(JOptionPane.showInputDialog("¿Tiene asientos de climatizador?"));
+		
+		System.out.println(micoche.dime_climatizador());
+		
+		System.out.println(micoche.dime_peso_coche());
+		
+		System.out.println("El precio final del coche es: "+ micoche.precio_coche());
+
 
 	}
 
---
+++ javaSE/PrimerosPasos/src/poo/Uso_Empleado.java
@@ -0,0 +1,23 @@
+package poo;
+import java.util.*;
+
+public class Uso_Empleado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Empleado {
+	
+	public Empleado(String nom, double sue, int agno, int mes, int dia) {
+		
+	}
+	
+	private String nombre;
+	private double sueldo;
+	private Date altaContrato;
+	
+}
